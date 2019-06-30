45165b2ad9983eb8767f48f7e39ccdef4f4faaa1
Noam <shvachernoam@gmail.com>
2019-06-30 21:23:31 +0300

    updated fix for heroku

--- app/helpers/fireBaseIndex.js
+++ app/helpers/fireBaseIndex.js
@@ -4,7 +4,11 @@ var firebase = require('firebase');
 require('firebase/auth');
 
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+    }),
     databaseURL: process.env.FIREBASE_DB_URL
 });
 
